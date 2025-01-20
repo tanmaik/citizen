683d809a3ba38ea26e8d2a212e04ba8b1562cdef
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-01-19 23:04:35 -0500

    design header

---
+++ web/app/components/header.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function Header() {
+  const [weather, setWeather] = useState({ description: "", temperature: "" });
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const { latitude, longitude } = position.coords;
+      try {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY}`
+        );
+        const data = await response.json();
+        if (data.weather && data.weather.length > 0) {
+          setWeather({
+            description: data.weather[0].description,
+            temperature: `${Math.round(data.main.temp)}°F`,
+          });
+        }
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      }
+    });
+  }, []);
+
+  return (
+    <nav className="flex justify-between items-end py-4 border-b">
+      <div className="text-left">
+        <h1 className="font-bold">pulse</h1>
+        <p>
+          {new Date()
+            .toLocaleDateString("en-US", {
+              weekday: "long",
+              month: "long",
+              day: "numeric",
+            })
+            .toLowerCase()}
+        </p>
+      </div>
+      <div>
+        <p className="text-right">
+          {weather.description}
+          <br />
+          {weather.temperature}
+        </p>
+      </div>
+    </nav>
+  );
+}
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,18 +1,10 @@
+import Header from "./components/header";
+
 export default function Home() {
   return (
-    <div className="justify-center flex py-5 ">
+    <div className="flex justify-center">
       <div className="w-full max-w-4xl">
-        <div className="">
-          <h1 className="text-center font-semibold text-4xl">pulse</h1>
-          <p className="text-center mt-2 font-medium">
-            {new Date().toLocaleDateString("en-US", {
-              weekday: "long",
-              month: "long",
-              day: "numeric",
-            })}
-          </p>
-        </div>
-        <hr className="mt-4" />
+        <Header />
       </div>
     </div>
   );

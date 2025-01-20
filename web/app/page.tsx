b3b48f52a4da53e19c5fce0c0597af5204d46aaa
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-01-19 22:47:40 -0500

    fix typescript error

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,16 +1,15 @@
 export default function Home() {
-  const formatDate = (date) => {
-    const options = { weekday: 'long', month: 'long', day: 'numeric' };
-    return date.toLocaleDateString('en-US', options);
-  };
-
   return (
     <div className="justify-center flex py-5 ">
       <div className="w-full max-w-4xl">
         <div className="">
           <h1 className="text-center font-semibold text-4xl">pulse</h1>
           <p className="text-center mt-2 font-medium">
-          {formatDate(new Date())}
+            {new Date().toLocaleDateString("en-US", {
+              weekday: "long",
+              month: "long",
+              day: "numeric",
+            })}
           </p>
         </div>
         <hr className="mt-4" />

669a351ed0cae940b75e1840149ff2efbb8128bf
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-01-19 23:07:55 -0500

    padding for small mobile

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -3,7 +3,7 @@ import Header from "./components/header";
 export default function Home() {
   return (
     <div className="flex justify-center">
-      <div className="w-full max-w-4xl">
+      <div className="w-full px-4 max-w-4xl">
         <Header />
       </div>
     </div>

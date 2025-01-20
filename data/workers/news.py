2b600927d413db981fac58466e736ecdfdf02998
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-01-19 21:46:31 -0500

    page

--- data/workers/news.py
+++
@@ -1,8 +0,0 @@
-from newspaper import Article
-
-def get_news(url):
-    article = Article(url)
-    article.download()
-    article.parse()
-    return article.text
-
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="justify-center flex py-5 ">
       <div className="w-full max-w-2xl">
-        <h1 className="text-center font-bold text-8xl ">pulse</h1>
+        <h1 className="text-center font-bold text-8xl">pulse</h1>
       </div>
     </div>
   );
